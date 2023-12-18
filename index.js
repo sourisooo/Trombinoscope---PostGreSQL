39fae43c5490a85cc71f9cb6af3f789762151141
sourisooo <119761249+sourisooo@users.noreply.github.com>
2023-12-18 18:12:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,16 @@ server.set('view engine', 'ejs');
 
 server.use(express.urlencoded({ extended: true }));
 
+const session = require('express-session');
+server.use(session({
+    secret: 'keyboard catLMkfmlkl smldfkmùlkdmùksqùmlkd lkmlsdfkùlks', // une phrase le plus possible pour faire le plus complexe possible afin de générer le session ID
+    
+    // 2 paramètres obligatoires !
+    resave: false, // force un enregistrement de notre session même si elle n'est pas modifiée
+    saveUninitialized: true // force un enregistrement d'une session non initialisée
+}));
+
+
 // Ajout du router
 const router = require("./app/router");
 server.use(router);
